ff2a9bc7006b06ae82a294d71d1e42dcf65458b7
zyl <744891759@qq.com>
2022-10-05 20:32:42 +0800

    dew

---
+++ assets/index.0c0fb37e.js
@@ -0,0 +1,27 @@
+import{n as e,u as a,R as n,U as i,r,j as t}from"./index.b1661de4.js";const o=e.div`
+  position: relative;
+  width: 100%;
+  box-sizing: border-box;
+`,s=e(o)`
+  height: 100vh;
+  background: rgb(242, 245, 249);
+`,d=e(o)`
+  width: 90vw;
+  max-width: 600px;
+  min-width: 300px;
+  padding: 36px;
+  border-radius: 10px;
+  background: white;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+`;e.h2`
+  text-align: center;
+`;e.p`
+  text-align: center;
+  color: rgba(34, 51, 84, 0.7);
+`;e.span`
+  color:#1976d2
+`;e.p`
+  color:#1976d2
+`;const p=()=>(a(),n(i),r.exports.useState({userName:"",passWord:""}),r.exports.useState(!1),t(s,{children:t(d,{children:t("p",{children:"1"})})}));export{p as default};
